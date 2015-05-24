7c347af699b564c693d183df62616be5f707bdb6
Najnin Ahmed <najnin14jan@gmail.com>
2015-05-24 12:29:45 +0600

    Controlling monkey by cursors

--- js/main.js
+++ js/main.js
@@ -12,32 +12,50 @@ var mainState = {
     },
 
     create: function() { 
-        
         this.game.physics.startSystem(Phaser.Physics.ARCADE)
         this.sky = this.game.add.sprite(0,0,"sky")
         this.sky.scale.setTo(2,2)
         this.tree = this.game.add.sprite(0,0,"tree")
         this.monk = this.game.add.sprite(50,1000,'monkey')
-        
         this.ground = this.game.add.sprite(0,1200,'platform')
-        this.ground.scale.setTo(2,1);
-        
+        this.ground.scale.setTo(1.8,0.8);
         this.monk.animations.add('game',[0,1,2], 5, true);
         this.monk.animations.play('game')
         game.physics.arcade.enable(this.tree)
         game.physics.arcade.enable(this.monk)
         game.physics.arcade.enable(this.ground)
-        this.monk.body.gravity.y = -50;
+        this.monk.body.gravity.y = 300;
         this.monk.body.collideWorldBounds = true;
         this.ground.body.gravity.y = 300;
         this.ground.body.immovable = true;
         this.ground.body.collideWorldBounds = true;
-        
-    },
+     },
 
     update: function() {
         game.physics.arcade.collide(this.monk, this.ground);
+        cursors = game.input.keyboard.createCursorKeys();
+        this.monk.body.velocity.x=0;
+    if (cursors.left.isDown)
+    {
+        this.monk.body.velocity.x = -350;
+        this.monk.animations.play('game');
+    }
+    else if (cursors.right.isDown)
+    {
+        this.monk.body.velocity.x = 350;
+        this.monk.animations.play('game');
+    }
+    else
+    {
+        this.monk.animations.stop();
+        this.monk.frame = 2;
+    }
+    if (cursors.up.isDown && this.monk.body.touching.down)
+    {
+        this.monk.body.velocity.y = -350;
+    }
     },
+
     jump: function() { 
     	    
     },
